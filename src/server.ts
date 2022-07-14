456d3c2b7dda98e34c81297ce2468e1ee189aba2
Shalior <aliali.t2y@gmail.com>
2022-07-15 02:34:02 +0430

    Refresh database on tests running

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,4 @@ import app from './app';
 import { shutdownable } from './shutdown';
 
 export default shutdownable(http.createServer(app));
+
--- tests/_hooks.ts
+++ tests/_hooks.ts
@@ -1,16 +1,27 @@
-import { start, stop } from '../src/lifecycle';
-import { nullary } from '../src/algebra/functions';
+import {start, stop} from '../src/lifecycle';
+import {nullary} from '../src/algebra/functions';
+import db from "../src/db";
 
 type DoneFunction = () => {};
 
 export const mochaHooks = {
 	beforeAll(done: DoneFunction) {
-		start({ queues: false, server: true })
-			.then(() => done())
+		start({queues: false, server: true})
+			.then(() => {
+				db.migrate.latest().then(() => {
+					done()
+				})
+			})
 			.catch(err => console.log(err));
 	},
+
 	afterAll(done: DoneFunction) {
-		stop()
-			.then(() => done());
+		db.migrate.rollback(undefined, true).then(() => {
+			stop()
+				.then(() => {
+					done();
+				}).catch(err => console.log(err));
+		}).catch(err => console.log(err));
+
 	},
 };
