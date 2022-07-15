c77bf41f7089ff3625733b08321e1e3dd3456eb3
Shalior <aliali.t2y@gmail.com>
2022-07-15 10:42:02 +0430

    wip

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ const config = {
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 	},
-	debug: ['development', 'test'].includes(process.env.NODE_ENV),
+	debug: ['development'].includes(process.env.NODE_ENV),
 	pool: {
 		min: 2,
 		max: 10,
