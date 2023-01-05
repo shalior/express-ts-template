0d2078900f5273f66c14185eacd5a007d8d665a1
Shalior <ali_shalior@proton.me>
2023-01-05 22:57:20 +0330

    Fix UserService imports

--- src/routes/app/user.ts
+++ src/routes/app/user.ts
@@ -1,12 +1,12 @@
 import { asyncWrapper } from '@cdellacqua/express-async-wrapper';
 import { Router } from 'express';
 import { HttpStatus } from '../../http/status';
-import { generateAuthResponse } from '../../services/UserService';
+import UserService from '../../services/UserService';
 
 const r: Router = Router();
 export default r;
 
 r.post('/user', asyncWrapper(async (req, res) => {
-	const authResponse = generateAuthResponse(res.locals.user);
+	const authResponse = UserService.generateAuthResponse(res.locals.user);
 	res.status(HttpStatus.Created).json(authResponse);
 }));
