a64ab95881c5726bb94728aadfc66f6619de9a37
Shalior <ali_shalior@proton.me>
2023-03-11 11:20:41 +0330

    fix eslint

--- docs/generate.js
+++ docs/generate.js
@@ -1,71 +1,81 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+	function adopt(value) { return value instanceof P ? value : new P((resolve) => { resolve(value); }); }
+	return new (P || (P = Promise))((resolve, reject) => {
+		function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+		function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+		function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+		step((generator = generator.apply(thisArg, _arguments || [])).next());
+	});
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
+const __generator = (this && this.__generator) || function (thisArg, body) {
+	let _ = {
+		label: 0, sent() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [],
+	}; let f; let y; let t; let
+		g;
+	return g = { next: verb(0), throw: verb(1), return: verb(2) }, typeof Symbol === 'function' && (g[Symbol.iterator] = function () { return this; }), g;
+	function verb(n) { return function (v) { return step([n, v]); }; }
+	function step(op) {
+		if (f) throw new TypeError('Generator is already executing.');
+		while (_) {
+			try {
+				if (f = 1, y && (t = op[0] & 2 ? y.return : op[0] ? y.throw || ((t = y.return) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+				if (y = 0, t) op = [op[0] & 2, t.value];
+				switch (op[0]) {
+					case 0: case 1: t = op; break;
+					case 4: _.label++; return { value: op[1], done: false };
+					case 5: _.label++; y = op[1]; op = [0]; continue;
+					case 7: op = _.ops.pop(); _.trys.pop(); continue;
+					default:
+						if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+						if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+						if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+						if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+						if (t[2]) _.ops.pop();
+						_.trys.pop(); continue;
+				}
+				op = body.call(thisArg, _);
+			} catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+		}
+		if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+	}
 };
 exports.__esModule = true;
-var path_1 = require("path");
-var fs_1 = require("fs");
+const path_1 = require('path');
+const fs_1 = require('fs');
 // @ts-ignore
-var api_1 = require("./api");
-var Template = function (output, _a) {
-    var api = _a.api, title = _a.title, redoc = _a.redoc;
-    return fs_1.promises.writeFile(output, "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"UTF-8\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n\t\t<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />\n\t\t<title>".concat(title, "</title>\n\t</head>\n\t<body>\n\t\t<redoc spec-url=\"").concat(api, "\"></redoc>\n\t\t<script src=\"").concat(redoc, "\"></script>\n\t</body>\n</html>\n"));
+const api_1 = require('./api');
+
+const Template = function (output, _a) {
+	const { api } = _a;
+	const { title } = _a;
+	const { redoc } = _a;
+	return fs_1.promises.writeFile(output, '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="UTF-8" />\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n\t\t<meta http-equiv="X-UA-Compatible" content="ie=edge" />\n\t\t<title>'.concat(title, '</title>\n\t</head>\n\t<body>\n\t\t<redoc spec-url="').concat(api, '"></redoc>\n\t\t<script src="').concat(redoc, '"></script>\n\t</body>\n</html>\n'));
 };
-var Api = function (output) { return fs_1.promises.writeFile(output, JSON.stringify(api_1["default"])); };
-var Redoc = function (output) { return fs_1.promises.copyFile((0, path_1.join)((0, path_1.dirname)(require.resolve('redoc')), 'redoc.standalone.js'), output); };
-exports["default"] = (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var out, apiFile, redocFile;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                out = (0, path_1.join)(__dirname, 'static');
-                apiFile = 'api.json';
-                redocFile = 'redoc.js';
-                return [4 /*yield*/, fs_1.promises.mkdir(out, { recursive: true })];
-            case 1:
-                _a.sent();
-                return [2 /*return*/, Promise.all([
-                        Api((0, path_1.join)(out, apiFile)),
-                        Redoc((0, path_1.join)(out, redocFile)),
-                        Template((0, path_1.join)(out, 'index.html'), {
-                            api: apiFile,
-                            title: api_1["default"].info.title,
-                            redoc: redocFile
-                        }),
-                    ])];
-        }
-    });
-}); })();
+const Api = function (output) { return fs_1.promises.writeFile(output, JSON.stringify(api_1.default)); };
+const Redoc = function (output) { return fs_1.promises.copyFile((0, path_1.join)((0, path_1.dirname)(require.resolve('redoc')), 'redoc.standalone.js'), output); };
+exports.default = (function () {
+	return __awaiter(void 0, void 0, void 0, function () {
+		let out; let apiFile; let
+			redocFile;
+		return __generator(this, (_a) => {
+			switch (_a.label) {
+				case 0:
+					out = (0, path_1.join)(__dirname, 'static');
+					apiFile = 'api.json';
+					redocFile = 'redoc.js';
+					return [4 /* yield */, fs_1.promises.mkdir(out, { recursive: true })];
+				case 1:
+					_a.sent();
+					return [2 /* return */, Promise.all([
+						Api((0, path_1.join)(out, apiFile)),
+						Redoc((0, path_1.join)(out, redocFile)),
+						Template((0, path_1.join)(out, 'index.html'), {
+							api: apiFile,
+							title: api_1.default.info.title,
+							redoc: redocFile,
+						}),
+					])];
+			}
+		});
+	});
+}());
